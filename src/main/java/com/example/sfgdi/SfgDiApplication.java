8c53305e4c8cfbb9fbe24ee9abae2d6bb3daecf9
Merry Damsko <m.d4msk0@gmail.com>
2024-04-24 23:51:35 +0200

    unused import

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -6,7 +6,6 @@ import com.example.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class SfgDiApplication {
